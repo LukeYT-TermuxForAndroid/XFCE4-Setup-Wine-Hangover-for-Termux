992bc1bda039ac4d9366e3755f43e22748b24b03
LukeYT PvZ2PAK <phamnguyengiathinh89@gmail.com>
2026-05-26 18:42:29 +0700

    Update wine_hangover.sh

--- setup/wine_hangover.sh
+++ setup/wine_hangover.sh
@@ -6,9 +6,10 @@ echo -e "\e[32mInstalling Wine Hangover\e[0m"
 pkg install hangover*
 wget -O wine.tar.xz https://github.com/alexvorxx/hangover-termux/releases/download/9.22/wine_hangover_9.22_bionic_build_patched.tar.xz
 tar -xf wine.tar.xz
-mv wine_hangover/arm64-v8a/bin /data/data/com.termux/files/usr/
-mv wine_hangover/arm64-v8a/lib /data/data/com.termux/files/usr/
-mv wine_hangover/arm64-v8a/share /data/data/com.termux/files/usr/
+cd wine_hangover/arm64-v8a
+mv bin/ /data/data/com.termux/files/usr/
+mv lib/ /data/data/com.termux/files/usr/
+mv share/ /data/data/com.termux/files/usr/
 clear
 echo -e "\e[32mRunning Wine Explorer\e[0m"
 wine explorer
